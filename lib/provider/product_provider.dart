149cd257868fb33f80a5c7ada1fb6a45ea68b70d
Carlos Vinicius Rocha Guedes <c.vinicius.gr@gmail.com>
2023-07-03 00:32:01 -0300

    atualização 03/07 00:31

--- lib/provider/product_provider.dart
+++ lib/provider/product_provider.dart
@@ -10,6 +10,8 @@ class ProductProvider with ChangeNotifier {
     String? category,
     String? description,
     List<String>? imageUrlList,
+    bool? chargeShipping,
+    int? shippingCharge,
   }) {
     if (productName != null) {
       productData['productName'] = productName;
@@ -28,6 +30,12 @@ class ProductProvider with ChangeNotifier {
     }
     if (imageUrlList != null) {
       productData['imageUrlList'] = imageUrlList;
+    }
+    if (chargeShipping != null) {
+      productData['chargeShipping'] = chargeShipping;
+    }
+    if (shippingCharge != null) {
+      productData['shippingCharge'] = shippingCharge;
     }
   }
 }
--- lib/vendor/views/screens/upload_tab_screens/shipping_screen.dart
+++ lib/vendor/views/screens/upload_tab_screens/shipping_screen.dart
@@ -1,12 +1,48 @@
+import 'package:agro_market/provider/product_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ShippingScreen extends StatelessWidget {
-  const ShippingScreen({super.key});
+class ShippingScreen extends StatefulWidget {
+  @override
+  State<ShippingScreen> createState() => _ShippingScreenState();
+}
+
+class _ShippingScreenState extends State<ShippingScreen> {
+  bool? _chargeShipping = false;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Entrega'),
+    final ProductProvider _productProvider =
+        Provider.of<ProductProvider>(context);
+    return Column(
+      children: [
+        CheckboxListTile(
+          title: Text(
+            'Cobrar Entrega',
+            style: TextStyle(fontSize: 18),
+          ),
+          value: _chargeShipping,
+          onChanged: (value) {
+            setState(() {
+              _chargeShipping = value;
+              _productProvider.getFormData(chargeShipping: _chargeShipping);
+            });
+          },
+        ),
+        if (_chargeShipping == true)
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              onChanged: (value) {
+                _productProvider.getFormData(
+                  shippingCharge: int.parse(value),
+                );
+              },
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Taxa de Envio'),
+            ),
+          ),
+      ],
     );
   }
 }
